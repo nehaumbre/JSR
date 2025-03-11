644e29bd07e8ed4cb0a180aab529eb59c118f288
nehaumbre <nehaumbre28@gmail.com>
2025-03-11 17:29:27 +0530

    Memoization
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

--- JS/memo.js
+++ JS/memo.js
@@ -6,3 +6,23 @@ function addTo80(n) {
 addTo80(5);
 addTo80(5);
 addTo80(5);
+// Bad way
+
+function memoizedoAddto80(){
+    let cache ={}
+    return function(n){ //closure to access cached object
+        if (n in cache){
+            return cache[n]
+        }else{
+            console.log('first..')
+            cache[n] = n + 80
+            return cache[n] 
+        }
+    }
+}
+
+const memoized = memoizedoAddto80()
+console.log('1', memoized(5))
+console.log('2', memoized(5))
+console.log('3', memoized(5))
+console.log('4', memoized(20))
